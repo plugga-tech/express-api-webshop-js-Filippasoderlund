7dcc33b4a10bc5e0d12f6587ffe907db088e1c09
Filippasoderlund <filippa.soderlund@medieinstitutet.se>
2023-04-05 12:26:59 +0200

    Products done

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const productSchema = require('../models/productModel.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('products', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const products = await productSchema.find()
+    res.send(products);
+})
+
+router.get('/:id', async function(req, res, next) {
+  const product = req.params.id;
+  const foundProduct = await productSchema.findOne({ _id: product});
+    res.send(foundProduct);
 });
+
+router.post('/add', async function(req, res, next) {
+  const products = await productSchema.create(req.body)
+    res.send(products);
+})
 
 module.exports = router;
